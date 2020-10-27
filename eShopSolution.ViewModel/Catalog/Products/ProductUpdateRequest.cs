7074619a53b178980ca45d1a103d4dc9b5e79ca3
LuongBaoTin <luongbaotin2019@gmail.com>
2020-10-27 17:28:15 +0700

    Swagger + Application

---
+++ eShopSolution.ViewModel/Catalog/Products/ProductUpdateRequest.cs
@@ -0,0 +1,16 @@
+﻿namespace eShopSolution.ViewModel.Catalog.Products
+{
+    public class ProductUpdateRequest
+    {
+        public int Id { set; get; }
+        public string Name { set; get; }
+        public string Description { set; get; }
+        public string Details { set; get; }
+        public string SeoDescription { set; get; }
+        public string SeoTitle { set; get; }
+
+        public string SeoAlias { get; set; }
+        public string LanguageId { set; get; }
+
+    }
+}
